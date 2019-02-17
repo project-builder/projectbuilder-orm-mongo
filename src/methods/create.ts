67184b9553cd5a0a557ab5cfcddf8243fdeb93fb
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-02-17 14:44:07 -0600

    blahj

--- src/methods/create.ts
+++ src/methods/create.ts
@@ -1,5 +1,7 @@
 const create = async function(){
 
+console.log('hit this mongo create file')
+
 const client = await this.MongoClient.connect(this.url, {useNewUrlParser: true });
 const db = await client.db(this.dbName);
 
